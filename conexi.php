31994af2dd4d82a4c514935e291c4acc2fe1927a
ferchuse <sistemas@glifo.mx>
2020-03-12 12:16:00 -0600

    Update conexi.php

--- conexi.php
+++ conexi.php
@@ -39,6 +39,8 @@
 		
 		if($_SERVER["SERVER_NAME"] != "localhost") {
 			mysqli_query($link, "SET NAMES 'utf8'") or die("Error Cambiando charset").mysqli_error($link);
+				
+			mysqli_query($link, "SET SESSION sql_mode = ''") or die("Error Cambiando charset").mysqli_error($link);
 			
 			
 			mysqli_query($link, "SET CHARACTER SET utf8") or die("Error en charset UTF8".mysqli_error($link));
