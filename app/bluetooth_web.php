e6a6fe9aa43f2e3dd6e354230b8caece7ad741a8
ferchuse <sistemas@glifo.mx>
2020-09-16 20:49:42 -0500

    Update bluetooth_web.php

--- app/bluetooth_web.php
+++ app/bluetooth_web.php
@@ -25,7 +25,13 @@
 			let button = document.getElementById("conectar");
 			
 			button.addEventListener('pointerup', function(event) {
-				navigator.bluetooth.requestDevice
+				navigator.bluetooth.requestDevice(options).then(function(device) {
+					console.log('Name: ' + device.name);
+					// Do something with the device.
+				})
+				.catch(function(error) {
+					console.log("Something went wrong. " + error);
+				});
 			});
 		</script>
 		
